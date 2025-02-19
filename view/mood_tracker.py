ca912237e27d8174ff0acbfec3f52b3e59e0bf19
Anastasia Klimson0051 <anastasia.klimson0051@stud.hkr.se>
2025-02-19 15:36:15 +0100

    Made Mood tracker logic work as it should

--- view/mood_tracker.py
+++ view/mood_tracker.py
@@ -13,16 +13,16 @@ class MoodTrackerWindow(QMainWindow):
         self.mood_next_question1 = self.findChild(QtWidgets.QPushButton, "mood_next_question1")
         self.mood_back_question1 = self.findChild(QtWidgets.QPushButton, "mood_back_question1")
 
-        #self.mood_question1 = self.findChild(QtWidgets.QFrame, "mood_question1_frame")
-        #self.mood_question2 = self.findChild(QtWidgets.QFrame, "mood_question2_frame")
-        # self.mood_question3 = self.findChild(QtWidgets.QFrame, "mood_question3_frame")
+        self.mood_question1 = self.findChild(QtWidgets.QFrame, "mood_question1_frame")
+        self.mood_question2 = self.findChild(QtWidgets.QFrame, "mood_question2_frame")
+        self.mood_question3 = self.findChild(QtWidgets.QFrame, "mood_question3_frame")
 
-        #self.mood_question1.show()
-        #self.mood_question2.hide()
-        # self.mood_question3.hide()
+        self.mood_question1.show()
+        self.mood_question2.hide()
+        self.mood_question3.hide()
 
-        #self.mood_next_question1.clicked.connect(self.show_question2)
-        # self.mood_next_question2.clicked.connect(self.show_question3)
+        self.mood_next_question1.clicked.connect(self.show_question2)
+        self.mood_next_question2.clicked.connect(self.show_question3)
         self.mood_back_question1.clicked.connect(self.go_back_to_menu)
 
         # QUESTION 1
@@ -49,11 +49,12 @@ class MoodTrackerWindow(QMainWindow):
         }
 
         # QUESTION 2
-        self.mood_school = self.findChild(QtWidgets.QPushButton, "mood_school")
-        self.mood_work = self.findChild(QtWidgets.QPushButton, "mood_work")
-        self.mood_relationship = self.findChild(QtWidgets.QPushButton, "mood_relationship")
-        self.mood_weather = self.findChild(QtWidgets.QPushButton, "mood_weather")
-        self.mood_health = self.findChild(QtWidgets.QPushButton, "mood_health")
+        self.mood_school = self.findChild(QtWidgets.QRadioButton, "mood_school")
+        self.mood_work = self.findChild(QtWidgets.QRadioButton, "mood_work")
+        self.mood_relationship = self.findChild(QtWidgets.QRadioButton, "mood_relationship")
+        self.mood_weather = self.findChild(QtWidgets.QRadioButton, "mood_weather")
+        self.mood_health = self.findChild(QtWidgets.QRadioButton, "mood_health")
+        self.mood_sleep = self.findChild(QtWidgets.QRadioButton, "mood_sleep")
 
         self.mood_back_question2 = self.findChild(QtWidgets.QPushButton, "mood_back_question2")
         self.mood_next_question2 = self.findChild(QtWidgets.QPushButton, "mood_next_question2")
@@ -63,9 +64,56 @@ class MoodTrackerWindow(QMainWindow):
         self.mood_relationship.clicked.connect(lambda: self.answer_question2("Relationship"))
         self.mood_weather.clicked.connect(lambda: self.answer_question2("Weather"))
         self.mood_health.clicked.connect(lambda: self.answer_question2("Health"))
+        self.mood_sleep.clicked.connect(lambda: self.answer_question2("Sleep"))
 
         self.mood_back_question2.clicked.connect(self.show_question1)
         self.mood_next_question2.clicked.connect(self.show_question3)
+
+        # QUESTION 3
+        self.mood_music = self.findChild(QtWidgets.QRadioButton, "mood_music")
+        self.mood_enjoy = self.findChild(QtWidgets.QRadioButton, "mood_enjoy")
+        self.mood_rest = self.findChild(QtWidgets.QRadioButton, "mood_rest")
+        self.mood_talk = self.findChild(QtWidgets.QRadioButton, "mood_talk")
+        self.mood_walk = self.findChild(QtWidgets.QRadioButton, "mood_walk")
+
+        self.mood_back_question3 = self.findChild(QtWidgets.QPushButton, "mood_back_question3")
+        self.mood_next_question3 = self.findChild(QtWidgets.QPushButton, "mood_next_question3")
+
+        self.mood_music.clicked.connect(lambda: self.answer_question3("Music"))
+        self.mood_enjoy.clicked.connect(lambda: self.answer_question3("Enjoy"))
+        self.mood_rest.clicked.connect(lambda: self.answer_question3("Rest"))
+        self.mood_talk.clicked.connect(lambda: self.answer_question3("Talk"))
+        self.mood_walk.clicked.connect(lambda: self.answer_question3("Walk"))
+
+        self.mood_back_question3.clicked.connect(self.show_question2)
+        self.mood_next_question3.clicked.connect(self.finish_mood_tracker)
+
+        # TIPS AND QUOTES
+        self.tips_and_quotes = {
+            "Music": {
+                "tip": "Listening to your favorite songs can relax your mind and improve your mood.",
+                "quote": "Where words fail, music speaks."
+            },
+            "Enjoy": {
+                "tip": "Do something you enjoy today, like watching a movie or reading a book.",
+                "quote": "Happiness is not something ready-made. It comes from your own actions."
+            },
+            "Rest": {
+                "tip": "Take some time to rest. A short nap or just relaxing can help you recharge.",
+                "quote": "Resting is not laziness, it is medicine for the soul."
+            },
+            "Talk": {
+                "tip": "Reach out to a friend or family member. Talking about your feelings can help a lot.",
+                "quote": "A burden shared is a burden halved."
+            },
+            "Walk": {
+                "tip": "Go for a short walk outside. Fresh air and movement can clear your mind.",
+                "quote": "Every step you take is a step closer to a better mood."
+            }
+        }
+
+        self.mood_tip_label = self.findChild(QtWidgets.QLabel, "mood_tip_label")
+        self.mood_quote_label = self.findChild(QtWidgets.QLabel, "mood_quote_label")
 
         # self.db = Write_db()
 
@@ -80,24 +128,41 @@ class MoodTrackerWindow(QMainWindow):
         #    "answer": rating
         #})
 
-    #def show_question2(self):
-        #self.mood_question1.hide()
-        #self.mood_question2.show()
-
+        # In the question 1
     def go_back_to_menu(self):
         from view.menu import MenuWindow
         self.menu_window = MenuWindow()
         self.menu_window.show()
         self.close()  # Closes Mood tracker window
 
-    #def show_question3(self):
-        #self.mood_question2.hide()
-        #self.mood_question3.show()
+    def show_question1(self):
+        self.mood_question1.show()
+        self.mood_question2.hide()
+        self.mood_question3.hide()
+
+    def show_question2(self):
+        self.mood_question1.hide()
+        self.mood_question2.show()
+        self.mood_question3.hide()
+
+    def show_question3(self):
+        self.mood_question1.hide()
+        self.mood_question2.hide()
+        self.mood_question3.show()
 
     def answer_question2(self, answer):
         print(f"User selected: {answer} for question 2")
-        # Saving to Firebase will come later...
 
-    #def show_question1(self):
-        #self.mood_question2.hide()
-        #self.mood_question1.show()
+    def answer_question3(self, answer):
+        print(f"User selected: {answer} for question 3")
+
+        if answer in self.tips_and_quotes:
+            self.mood_tip_label.setText(self.tips_and_quotes[answer]["tip"])
+            self.mood_quote_label.setText(self.tips_and_quotes[answer]["quote"])
+
+        # After question 3
+    def finish_mood_tracker(self):
+        from view.menu import MenuWindow
+        self.menu_window = MenuWindow()
+        self.menu_window.show()
+        self.close()  # Closes Mood tracker window
